2a72b4fa122dbc2bfd6a20c40e63b980775420dd
Dominic Salvador <dsal.7k@gmail.com>
2022-07-03 03:42:03 +0800

    main python program

---
+++ super_ai_print.py
@@ -0,0 +1,17 @@
+import time
+
+# Simulate CPU Thinking
+print("Loading:")
+print("[=---------]\t10%")
+time.sleep(1)
+print("[==--------]\t20%")
+time.sleep(1)
+print("[=====-----]\t50%")
+time.sleep(1)
+print("[========--]\t80%")
+time.sleep(1)
+print("[==========]\t100%")
+print("Loading Done...")
+time.sleep(1)
+
+print("Hello Mom!")
